39cd2717835b42f5d089fd8be3eeb9ce5a084967
NikMine <46231395+NikMineZx@users.noreply.github.com>
2024-04-10 04:42:32 +0300

    Add my new projects

--- C#-code/Course/Course/obj/Release/net8.0/Course.AssemblyInfo.cs
+++ C#-code/Course/Course/obj/Release/net8.0/Course.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87513aa7520719ecff92e784d10ff7248f52864e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course")]
